94c06540cade12454c75353107193ef6ddc4c6bd
Wassem Tenbakji <wasee.tenbakji@gmail.com>
2025-07-12 18:17:45 +0300

    Make fields in PharmacyCreateRequestDTO required

--- src/main/java/com/Teryaq/user/dto/PharmacyCreateRequestDTO.java
+++ src/main/java/com/Teryaq/user/dto/PharmacyCreateRequestDTO.java
@@ -1,11 +1,16 @@
 package com.Teryaq.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class PharmacyCreateRequestDTO {
+    @NotBlank(message = "Pharmacy name couldn't be blank")
     private String pharmacyName;
+    @NotBlank(message = "License number couldn't be blank")
     private String licenseNumber;
+    @NotBlank(message = "Phone number couldn't be blank")
     private String phoneNumber;
+    @NotBlank(message = "Manager password couldn't be blank")
     private String managerPassword;
 }
